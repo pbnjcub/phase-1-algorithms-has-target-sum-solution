fb4fc794c8cce7422bc28d41ea4ff8925b8f4893
Paul Jung <pbnjcub@gmail.com>
2022-08-02 12:45:54 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-  //   iterate through each number in the array
-  for (let i = 0; i < array.length; i++) {
-  //   for the current num, identify a complement that adds to the target (comp = target - num)
-    const complement = target - array[i]
-  //   iterate through the rest of the array
-      for (let j = i + 1; j < array.length; j++) {
-      //     check if any number is our complement
-      //     if so, return true
-      if (array[j] === complement) return true
-      }
-
-    }
-return false
+  const seenNumbers = {}
+  for(const number of array) {
+    console.log(seenNumbers)
+    const complement = target - number
+      if (complement in seenNumbers) return true
+      seenNumbers[number] = true
+  }
+  return false
 }
+
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     // n steps
+//     const complement = target - array[i]
+//       for (let j = i + 1; j < array.length; j++) {
+//         // n * n steps
+//       if (array[j] === complement) return true
+//       }
+
+//     }
+// return false
+// }
 
 /* 
   Write the Big O time complexity of your function here
+  runtime: O(n)
+  Space: O(n)
 */
 
 /* 
+hasTargetSum([1, 2, 3, 4], 6)
+seenNumbers = {
+  1: true,
+  2: true,
+  3: true
+}
   Add your pseudocode here
   hasTargetSum([22, 19, 4, 6, 30], 25))
+  create an object to keep track of numbers we've already seen
   iterate through each number in the array
     for the current num, identify a complement that adds to the target (comp = target - num)
-    iterate through the rest of the array
-      check if any number is our complement
-      if so, return true
+    check if any key on our object is the complement
 
   if I reach the end of the array, return false
 
